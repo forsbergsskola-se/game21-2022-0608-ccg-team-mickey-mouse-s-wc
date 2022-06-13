34882c396371aba2b212f2afcb3c1b9715e92345
Oliver Källerfors <idetorka@hotmail.com>
2022-06-13 15:55:05 +0200

    Changed FamilyType To Alignment

--- Mickey Mouse Garden/Assets/Scripts/Meta/FighterSO.cs
+++ Mickey Mouse Garden/Assets/Scripts/Meta/FighterSO.cs
@@ -10,11 +10,12 @@ using UnityEngine.UI;
 public class FighterSO : ScriptableObject{
     [Header("Displayable Information")]
     public string Name;
-    public FamilyType Type;
+    public Alignment Alignment;
     public Rarity Rarity;
     [Min(1)]public int Level;
     [Min(0)]public float Attack;
     [Min(1)]public float MaxHealth;
     [Min(1)]public float Speed;
     public Image FighterImage; //TODO: Sprite or image?
+    
 }
