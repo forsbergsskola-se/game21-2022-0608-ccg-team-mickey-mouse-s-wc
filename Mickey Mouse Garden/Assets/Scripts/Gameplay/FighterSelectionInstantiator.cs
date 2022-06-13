24e202a7b2c3ceaf0a44c543ec6585795d20cddf
Benjamin Ting <tingzhb@gmail.com>
2022-06-13 14:32:29 +0200

    Add Fighter Properties to card prefab

--- Mickey Mouse Garden/Assets/Scripts/Gameplay/FighterSelectionInstantiator.cs
+++ Mickey Mouse Garden/Assets/Scripts/Gameplay/FighterSelectionInstantiator.cs
@@ -1,14 +1,19 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FighterSelectionInstantiator : MonoBehaviour{
 	private int id, health, damage, speed, level;
 	private string rarity, name, alignment;
-	private Sprite sprite;
-	private void Awake(){
+	private Sprite fighterSprite;
+
+	public TextMeshProUGUI nameText, rarityText, alignmentText, levelText, damageText, healthText, speedText;
+	public Image fighterImage;
+	private void Awake() {
 		Broker.Subscribe<FighterMessage>(OnFighterMessageReceived);
 	}
 
-	private void OnFighterMessageReceived(FighterMessage fighterMessage){
+	private void OnFighterMessageReceived(FighterMessage fighterMessage) {
 		id = fighterMessage.ID;
 		health = fighterMessage.Health;
 		damage = fighterMessage.Damage;
@@ -17,6 +22,18 @@ public class FighterSelectionInstantiator : MonoBehaviour{
 		rarity = fighterMessage.Rarity;
 		name = fighterMessage.Name;
 		alignment = fighterMessage.Alignment;
-		sprite = fighterMessage.Sprite;
+		fighterSprite = fighterMessage.Sprite;
+		AssignProperties();
+	}
+
+	private void AssignProperties() {
+		nameText.text = name;
+		rarityText.text = rarity;
+		alignmentText.text = alignment;
+		levelText.text = level.ToString();
+		damageText.text = damage.ToString();
+		healthText.text = health.ToString();
+		speedText.text = speed.ToString();
+		fighterImage.sprite = fighterSprite;
 	}
 }
