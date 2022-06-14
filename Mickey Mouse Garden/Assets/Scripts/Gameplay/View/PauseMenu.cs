7b106c986d0a2254080108253a17228483232cbd
Jesper Danielsson <jesper.danielsson@student.forsbergsskola.se>
2022-06-14 10:45:50 +0200

    Very simple pausemenu implemented in the arena

--- Mickey Mouse Garden/Assets/Scripts/Gameplay/View/PauseMenu.cs
+++ Mickey Mouse Garden/Assets/Scripts/Gameplay/View/PauseMenu.cs
@@ -1,19 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour{
     public Canvas pauseMenu;
+    public Canvas pauseButton;
 
     public void Start(){
         pauseMenu.enabled = false;
     }
     public void Pause(){
         pauseMenu.enabled = true;
+        pauseButton.enabled = false;
         Debug.Log("paused");
     }
     public void UnPause(){
         Debug.Log("unpaused");
+        pauseButton.enabled = true;
         pauseMenu.enabled = false;
+    }
+    public void GoToMarket(){
+        SceneManager.LoadScene("Market");
+    }
+    public void MainMenu(){
+        SceneManager.LoadScene("MainMenu");
     }
 }
