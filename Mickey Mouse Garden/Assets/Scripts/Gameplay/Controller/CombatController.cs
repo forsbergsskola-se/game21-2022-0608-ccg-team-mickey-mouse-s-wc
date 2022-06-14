c63f4d2e6a78535c0788846175ee1de741c82ca5
Jesper Danielsson <jesper.danielsson@student.forsbergsskola.se>
2022-06-14 10:04:49 +0200

    Mockup of the two teams in the arena

---
+++ Mickey Mouse Garden/Assets/Scripts/Gameplay/Controller/CombatController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatController : MonoBehaviour{
+    private Fighter[] playerFighters;
+    private Fighter[] enemyFighters;
+    
+    //TODO: use the command-pattern for this!
+    
+}
