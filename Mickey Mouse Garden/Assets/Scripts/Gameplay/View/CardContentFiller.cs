6eba7fd4488dc43357df333caef760d02f795f73
Jesper Danielsson <jesper.danielsson@student.forsbergsskola.se>
2022-06-15 11:48:44 +0200

    Can create stat-correct cards on messages

--- Mickey Mouse Garden/Assets/Scripts/Gameplay/View/CardContentFiller.cs
+++ Mickey Mouse Garden/Assets/Scripts/Gameplay/View/CardContentFiller.cs
@@ -9,47 +9,18 @@ public class CardContentFiller : MonoBehaviour{
 	private Alignment alignment;
 	private Sprite fighterSprite;
 	
-	public int ID { get; set;}
-	public float MaxHealth { get; set;}
-	public float Attack { get; set;}
-	public float Speed { get; set;}
-	public int Level  { get; set;}
-	public string Rarity  { get; set;}
-	public string Name { get; set;}
-	public Alignment Alignment { get; set;}
-	public Sprite Sprite { get; set;}
-
 	public TextMeshProUGUI nameText, rarityText, alignmentText, levelText, attackText, healthText, speedText;
 	public Image fighterImage;
-
-	private FighterInfo fighterInfo = new FighterInfo();
-
-	private void Start(){
-		AssignProperties();
-		AssignTextFields();
-	}
-
-	private void AssignProperties(){
-		id = ID;
-		health = MaxHealth;
-		attack = Attack;
-		speed = Speed;
-		level = Level;
-		rarity = Rarity;
-		name = Name;
-		alignment = Alignment;
-		fighterSprite = Sprite;
-	}
-
-	private void AssignTextFields() {
-		nameText.text = name;
-		rarityText.text = rarity;
-		alignmentText.text = alignment.ToString(); //TODO: this is an enum..
-		levelText.text = level.ToString();
-		attackText.text = attack.ToString();
-		healthText.text = health.ToString();
-		speedText.text = speed.ToString();
-		fighterImage.sprite = fighterSprite;
+	
+	public void AssignTextFields(FighterInfo fighter){
+		nameText.text = fighter.Name;
+		rarityText.text = fighter.Rarity;
+		alignmentText.text = fighter.Alignment.ToString();
+		levelText.text = fighter.Level.ToString();
+		attackText.text = fighter.Attack.ToString();
+		healthText.text = fighter.MaxHealth.ToString();
+		speedText.text = fighter.Speed.ToString();
+		fighterImage.sprite = fighter.Sprite;
 	}
 
 	
--- Mickey Mouse Garden/Assets/Scripts/Gameplay/View/CardCreator.cs
+++ Mickey Mouse Garden/Assets/Scripts/Gameplay/View/CardCreator.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class CardCreator : MonoBehaviour{
     public GameObject card;
     private FighterInfo fighter;
-    private List<FighterInfo> fighters = new List<FighterInfo>();
+    private List<FighterInfo> fighters = new();
     private void Awake(){
         Broker.Subscribe<FighterMessage>(OnFighterMessageReceived);
     }
@@ -12,27 +12,13 @@ public class CardCreator : MonoBehaviour{
     private void OnFighterMessageReceived(FighterMessage obj){
         fighter = obj.fighterInfo;
         fighters.Add(fighter);
-    }
-
-    private void InstantiateAllFightersGathered(){
-        foreach (var fighter in fighters){
-            InstantiateFighter(fighter);
-        }
+        InstantiateFighter(fighter);
     }
 
     private void InstantiateFighter(FighterInfo fighter){
         Instantiate(card, new Vector3(0, 0, 0), Quaternion.identity);
         var componentInChildren = card.GetComponentInChildren<CardContentFiller>();
-        componentInChildren.Name = fighter.Name;
-        componentInChildren.ID = fighter.ID;
-        componentInChildren.MaxHealth = fighter.MaxHealth;
-        componentInChildren.Attack = fighter.Attack;
-        componentInChildren.Speed = fighter.Speed;
-        componentInChildren.Level = fighter.Level;
-        componentInChildren.Rarity = fighter.Rarity;
-        componentInChildren.Name = fighter.Name;
-        componentInChildren.Alignment = fighter.Alignment;
-        componentInChildren.Sprite = fighter.Sprite;
+        componentInChildren.AssignTextFields(fighter);
     }
 
     private void OnDestroy(){
