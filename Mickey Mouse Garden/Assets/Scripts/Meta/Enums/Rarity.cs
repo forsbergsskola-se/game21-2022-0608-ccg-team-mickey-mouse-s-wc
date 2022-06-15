e8195ef6f6f2566389dd6c01e41537da08fef55d
Jasmine Öhlin <jasmine.ohlin@student.forsbergsskola.se>
2022-06-15 10:33:22 +0200

    Legendary added to rarity enum

--- Mickey Mouse Garden/Assets/Scripts/Meta/Enums/Rarity.cs
+++ Mickey Mouse Garden/Assets/Scripts/Meta/Enums/Rarity.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum Rarity 
-{
-   Common = 1,
-   Rare = 25,
-   Epic = 50
+public enum Rarity {
+   Common,
+   Rare,
+   Epic,
+   Legendary
 }
