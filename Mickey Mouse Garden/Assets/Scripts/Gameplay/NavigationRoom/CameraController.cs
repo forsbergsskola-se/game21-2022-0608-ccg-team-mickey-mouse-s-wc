56fbd1de9b60baf4277f12c254188c579a053f54
Benjamin Ting <tingzhb@gmail.com>
2022-06-15 10:17:29 +0200

    Add Viewpoint Teleportation to Navigation Room

--- Mickey Mouse Garden/Assets/Scripts/Gameplay/NavigationRoom/CameraController.cs
+++ Mickey Mouse Garden/Assets/Scripts/Gameplay/NavigationRoom/CameraController.cs
@@ -1,25 +1,65 @@
 using UnityEngine;
 
 public class CameraController : MonoBehaviour {
-	public float rotationAngle = 30;
 	public GameObject clickable1, clickable2, clickable3;
+	public GameObject viewpoint1, viewpoint2, viewpoint3;
+	public GameObject lookAt1, lookAt2, lookAt3;
+	private int viewPointNumber = 1;
 	
-	private void Start(){
-		clickable1.GetComponent<ClickZoom>().zoomChangedEvent.AddListener(ZoomIn);
-		clickable2.GetComponent<ClickZoom>().zoomChangedEvent.AddListener(ZoomIn);
-		clickable3.GetComponent<ClickZoom>().zoomChangedEvent.AddListener(ZoomIn);
+	private void Awake(){
+		clickable1.GetComponent<ClickZoom>().zoomChangedEvent.AddListener(ZoomChanged);
+		clickable2.GetComponent<ClickZoom>().zoomChangedEvent.AddListener(ZoomChanged);
+		clickable3.GetComponent<ClickZoom>().zoomChangedEvent.AddListener(ZoomChanged);
 	}
 	
 	// Needs to change to fixed positions and angles in scene with smooth damp.
-	public void CameraRotateRight(){
-		transform.RotateAround(Vector3.zero, Vector3.down, rotationAngle);
-	}
-	public void CameraRotateLeft(){
-		transform.RotateAround(Vector3.zero, Vector3.up, rotationAngle);
+	
+	public void GoToRightViewPoint() {
+		switch (viewPointNumber) {
+			case 1:
+				transform.position = viewpoint2.transform.position;
+				transform.LookAt(lookAt2.transform.position);
+				viewPointNumber = 2;
+				break;
+			
+			case 2:
+				transform.position = viewpoint3.transform.position;
+				transform.LookAt(lookAt3.transform.position);
+				viewPointNumber = 3;
+				break;
+			
+			case 3:
+				transform.position = viewpoint1.transform.position;
+				transform.LookAt(lookAt1.transform.position);
+				viewPointNumber = 1;
+				break;
+		}
 	}
 	
-	// Used to transition to store or feature. May discard if navigation arrows are in world space.
-	private void ZoomIn(Vector3 position, int newZoom){
+	public void GoToRLeftViewPoint() {
+		switch (viewPointNumber) {
+			case 1:
+				transform.position = viewpoint3.transform.position;
+				transform.LookAt(lookAt3.transform.position);
+				viewPointNumber = 3;
+				break;
+			
+			case 2:
+				transform.position = viewpoint1.transform.position;
+				transform.LookAt(lookAt1.transform.position);
+				viewPointNumber = 1;
+				break;
+			
+			case 3:
+				transform.position = viewpoint2.transform.position;
+				transform.LookAt(lookAt2.transform.position);
+				viewPointNumber = 2;
+				break;
+		}
+	}
+	
+	// Used to transition to store or feature. 
+	private void ZoomChanged(Vector3 position, int newZoom){
 		Camera.main.transform.LookAt(position);
 		Camera.main.fieldOfView = newZoom;
 	}
