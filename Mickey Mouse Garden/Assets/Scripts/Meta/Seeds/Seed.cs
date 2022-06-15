5910cff54686d3121e65b902199d1098ba51f421
Jasmine Öhlin <jasmine.ohlin@student.forsbergsskola.se>
2022-06-16 00:47:02 +0200

    Growth time set by seed

--- Mickey Mouse Garden/Assets/Scripts/Meta/Seeds/Seed.cs
+++ Mickey Mouse Garden/Assets/Scripts/Meta/Seeds/Seed.cs
@@ -5,8 +5,17 @@ using UnityEngine;
 
 namespace Meta.Seeds {
     public abstract class Seed : MonoBehaviour, IInventoryItem {
-        [SerializeField] private Sprite inventorySprite;
+        [SerializeField] private Sprite inventorySprite;         //TODO: Hardcode inventory sprite based on rarity
         public Rarity rarity;
+        private float growthTime;
+        
+        public float GrowthTime {
+            get => growthTime;
+            set {
+                growthTime = value;
+                growthTime = SetGrowthTime();
+            }
+        }
 
         public Sprite InventorySprite {
             get => inventorySprite;
@@ -19,23 +28,14 @@ namespace Meta.Seeds {
             Destroy(gameObject);
         }
         
-        //TODO: Hardcode inventory sprite based on rarity
-        
-        //TODO: Implement things below when more info has been given by the designers
-        /*[SerializeField] private float growthTime;
-
-        public float GrowthTime {
-            get => growthTime;
-            set {
-                growthTime = value;
-                growthTime = SetGrowthTime();
-            }
-        }
-
         private float SetGrowthTime() {
-            switch (SeedQuality) {
-                //Return different times
-            }
-        }*/
+            return rarity switch {             //TODO: Check exact values with designers
+                Rarity.Common => 2,
+                Rarity.Rare => 3,
+                Rarity.Epic => 4,
+                Rarity.Legendary => 5,
+                _ => throw new ArgumentOutOfRangeException()
+            };
+        }
     }
 }
