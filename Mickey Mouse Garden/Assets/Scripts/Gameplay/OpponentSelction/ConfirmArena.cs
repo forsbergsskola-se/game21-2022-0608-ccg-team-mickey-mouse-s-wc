212a91e196e4f98869ff7bc3c9f580086e570084
Benjamin Ting <tingzhb@gmail.com>
2022-06-16 10:19:51 +0200

    Add Scene transitions to OpponentSelection

--- Mickey Mouse Garden/Assets/Scripts/Gameplay/OpponentSelction/ConfirmArena.cs
+++ Mickey Mouse Garden/Assets/Scripts/Gameplay/OpponentSelction/ConfirmArena.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.SceneManagement;
 
 public class ConfirmArena : MonoBehaviour{
 
@@ -9,10 +10,26 @@ public class ConfirmArena : MonoBehaviour{
 
 	// Invokes event when object is clicked or touched (needs testing).
 	private void OnMouseDown() {
-		if (!shown){
+		if (!shown) {
 			Debug.Log("Opponent!"); 
 			confirmationBox.SetActive(true);
 			shown = true;
 		}
+	}
+
+	public void AnswerYes(){
+		confirmationBox.SetActive(false);
+		shown = false;
+		GoToArena();
+	}
+
+	public void AnswerNo(){
+		confirmationBox.SetActive(false);
+		shown = false;
+	}
+
+	// TODO: Change to teambuilding scene
+	private void GoToArena(){
+		SceneManager.LoadScene("Arena");
 	}
 }
