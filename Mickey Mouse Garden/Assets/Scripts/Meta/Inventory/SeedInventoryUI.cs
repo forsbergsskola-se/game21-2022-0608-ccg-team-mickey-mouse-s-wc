8c4762a254891fb12e03ea3b5112ccbb6c55546d
Jasmine Öhlin <jasmine.ohlin@student.forsbergsskola.se>
2022-06-17 13:53:13 +0200

    Script SeedInventoryUI made more readable

--- Mickey Mouse Garden/Assets/Scripts/Meta/Inventory/SeedInventoryUI.cs
+++ Mickey Mouse Garden/Assets/Scripts/Meta/Inventory/SeedInventoryUI.cs
@@ -1,31 +1,29 @@
 using System;
+using System.Linq;
 using Meta.Seeds;
 using TMPro;
 using UnityEngine;
 
 namespace Meta.Inventory {
     public class SeedInventoryUI : MonoBehaviour {
+        [SerializeField] private GameObject growSlotItemParent;
+        [SerializeField] private GameObject[] growSlotPrefabs;
+        
         private SeedSlot[] seedSlots;
         private GrowSlot[] growSlots;
-        [SerializeField] private GameObject growSlotPrefab;
         
         private void Start() {
             seedSlots = GetComponentsInChildren<SeedSlot>();
         }
 
         public void UpdateSeedCount(int count, Rarity rarity) {
-            SeedSlot slotToUpdate = null;
-
-            for (int i = 0; i < seedSlots.Length; i++) {
-                if (seedSlots[i].Rarity == rarity) {
-                    slotToUpdate = seedSlots[i];
-                    break;
-                }
-            }
-            
+            SeedSlot slotToUpdate = seedSlots.FirstOrDefault(slot => slot.Rarity == rarity);
             slotToUpdate.UpdateCountText(count);
         }
 
-        //TODO: Set up icon correctly in inspector
+        public void PlantSeedOfType(Rarity rarity) {
+            var slotToInstantiate = growSlotPrefabs.First(prefab => prefab.GetComponent<GrowSlot>().rarityType == rarity);
+            Instantiate(slotToInstantiate, growSlotItemParent.transform, false);
+        }
     }
 }
