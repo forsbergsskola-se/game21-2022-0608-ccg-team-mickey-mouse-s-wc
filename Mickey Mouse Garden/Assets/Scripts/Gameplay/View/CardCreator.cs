5d7716acba329b2f525dd15d55e0df87c60af378
Jesper Danielsson <jesper.danielsson@student.forsbergsskola.se>
2022-06-20 15:22:39 +0200

    Base striking in combat is doable

--- Mickey Mouse Garden/Assets/Scripts/Gameplay/View/CardCreator.cs
+++ Mickey Mouse Garden/Assets/Scripts/Gameplay/View/CardCreator.cs
@@ -5,21 +5,34 @@ public class CardCreator : MonoBehaviour{
     public GameObject card;
     public Transform parent;
     private FighterInfo fighter;
-    private List<FighterInfo> fighters = new();
     private void Awake(){
         Broker.Subscribe<FighterMessage>(OnFighterMessageReceived);
     }
 
     private void OnFighterMessageReceived(FighterMessage obj){
         fighter = obj.fighterInfo;
-        fighters.Add(fighter);
         InstantiateFighter(fighter);
     }
 
     private void InstantiateFighter(FighterInfo fighter){
-        Instantiate(card, new Vector3(0, 0, 0), Quaternion.identity, parent);
-        var componentInChildren = card.GetComponentInChildren<CardContentFiller>();
+        var createdCard = Instantiate(card, new Vector3(0, 0, 0), Quaternion.identity, parent);
+        var componentInChildren = createdCard.GetComponentInChildren<CardContentFiller>();
+        var fighterInfo = createdCard.AddComponent<FighterInfo>();
+        FillInInfo(fighterInfo);
         componentInChildren.AssignTextFields(fighter);
+    }
+
+    private void FillInInfo(FighterInfo fighterInfo){
+        fighterInfo.Alignment = fighter.Alignment;
+        fighterInfo.Attack = fighter.Attack;
+        fighterInfo.Level = fighter.Level;
+        fighterInfo.Name = fighter.Name;
+        fighterInfo.Rarity = fighter.Rarity;
+        fighterInfo.Speed = fighter.Speed;
+        fighterInfo.Sprite = fighter.Sprite;
+        fighterInfo.ID = fighter.ID;
+        fighterInfo.MaxHealth = fighter.MaxHealth;
+
     }
 
     private void OnDestroy(){
