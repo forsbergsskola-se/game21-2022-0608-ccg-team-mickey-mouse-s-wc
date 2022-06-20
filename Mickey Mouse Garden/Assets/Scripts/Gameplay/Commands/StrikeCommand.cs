2b96245ef3806ad2b713eec5be95db67cded5ab6
Jesper Danielsson <jesper.danielsson@student.forsbergsskola.se>
2022-06-20 16:06:29 +0200

    Implemented logic for alignment during combat and created 2 diffrent fighters for testing

--- Mickey Mouse Garden/Assets/Scripts/Gameplay/Commands/StrikeCommand.cs
+++ Mickey Mouse Garden/Assets/Scripts/Gameplay/Commands/StrikeCommand.cs
@@ -5,17 +5,61 @@ public class StrikeCommand : ICommand{
     private FighterInfo target;
     private FighterInfo striker;
 
+    private float multiplier = 1f;
+
     public StrikeCommand(FighterInfo target, FighterInfo striker){
         this.target = target;
         this.striker = striker;
     }
     public void Execute(){
-        var targetHealth = target.MaxHealth;
-        var strikerDmg = striker.Attack;
-        targetHealth -= strikerDmg;
-        target.MaxHealth = targetHealth;
-        Debug.Log(targetHealth);
-        Debug.Log(strikerDmg);
+        Strike();
+    }
+
+    private void Strike(){
+        CheckAlignment();
+        target.MaxHealth -= striker.Attack * multiplier;
+        Debug.Log(target.MaxHealth);
+    }
+
+    private void CheckAlignment(){
+        if (target.Alignment == striker.Alignment){
+            return;
+        }
+        switch (striker.Alignment){
+            case Alignment.Paper:{
+                switch (target.Alignment){
+                    case Alignment.Rock:
+                        multiplier += 0.5f;
+                        break;
+                    case Alignment.Scissors:
+                        multiplier -= 0.5f;
+                        break;
+                }
+                break;
+            }
+            case Alignment.Rock:{
+                switch (target.Alignment){
+                    case Alignment.Scissors:
+                        multiplier += 0.5f;
+                        break;
+                    case Alignment.Paper:
+                        multiplier -= 0.5f;
+                        break;
+                }
+                break;
+            }
+            case Alignment.Scissors:{
+                switch (target.Alignment){
+                    case Alignment.Paper:
+                        multiplier += 0.5f;
+                        break;
+                    case Alignment.Rock:
+                        multiplier -= 0.5f;
+                        break;
+                }
+                break;
+            }
+        }
     }
 
     public void Undo(){
--- Mickey Mouse Garden/Assets/Scripts/Gameplay/FighterInfoTester.cs
+++ Mickey Mouse Garden/Assets/Scripts/Gameplay/FighterInfoTester.cs
@@ -6,7 +6,11 @@ using UnityEngine;
 public class FighterInfoTester : MonoBehaviour{
     private FighterMessage fighterMessage;
     public Sprite sprite;
-    private void Start()   {
+    private void Start(){
+        CreateFighter();
+    }
+
+    private void CreateFighter(){
         fighterMessage = new FighterMessage();
         var fighter = new FighterInfo();
         fighter.ID = 1;
@@ -20,8 +24,25 @@ public class FighterInfoTester : MonoBehaviour{
         fighter.Sprite = sprite;
         fighterMessage.fighterInfo = fighter;
     }
+
     [ContextMenu("Send Fighter Message")]
     public void SendFighterMessage(){
         Broker.InvokeSubscribers(typeof(FighterMessage),fighterMessage);
+        CreateFighter2();
+    }
+
+    private void CreateFighter2(){
+        fighterMessage = new FighterMessage();
+        var fighter = new FighterInfo();
+        fighter.ID = 2;
+        fighter.MaxHealth = 10;
+        fighter.Attack = 5;
+        fighter.Speed = 7;
+        fighter.Rarity = Rarity.Legendary;
+        fighter.Name = "Foo";
+        fighter.Level = 3;
+        fighter.Alignment = Alignment.Rock;
+        fighter.Sprite = sprite;
+        fighterMessage.fighterInfo = fighter;
     }
 }
