f3af5317699ce46e0880cbc4a71d9c478f742b49
Jesper Danielsson <jesper.danielsson@student.forsbergsskola.se>
2022-06-20 12:36:55 +0200

    tested to add another command for scalability and refactoring

---
+++ Mickey Mouse Garden/Assets/Scripts/Gameplay/Commands/StrikeCommand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+public class StrikeCommand : ICommand{
+    private GameObject target;
+    private GameObject striker;
+
+    public StrikeCommand(GameObject target, GameObject striker){
+        this.target = target;
+        this.striker = striker;
+    }
+    public void Execute(){
+        Debug.Log($"{striker} hit {target}");
+    }
+
+    public void Undo(){
+        throw new NotImplementedException();
+    }
+}
--- Mickey Mouse Garden/Assets/Scripts/Gameplay/Controller/CombatController.cs
+++ Mickey Mouse Garden/Assets/Scripts/Gameplay/Controller/CombatController.cs
@@ -1,22 +1,4 @@
-using System;
 using UnityEngine;
-
-public class StrikeCommand : ICommand{
-   private GameObject target;
-   private GameObject striker;
-
-   public StrikeCommand(GameObject target, GameObject striker){
-      this.target = target;
-      this.striker = striker;
-   }
-   public void Execute(){
-      Debug.Log($"{striker} hit {target}");
-   }
-
-   public void Undo(){
-      throw new NotImplementedException();
-   }
-}
 
 public class CombatController : MonoBehaviour{
    private GameObject[] playerFighters;
