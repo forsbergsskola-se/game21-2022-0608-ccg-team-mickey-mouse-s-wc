342fffd2d58b9e64bc56271b202aabba287e6812
Jasmine Öhlin <jasmine.ohlin@student.forsbergsskola.se>
2022-06-20 15:37:53 +0200

    Update FighterInventory.cs

--- Mickey Mouse Garden/Assets/Scripts/Meta/Inventory/FighterInventory/FighterInventory.cs
+++ Mickey Mouse Garden/Assets/Scripts/Meta/Inventory/FighterInventory/FighterInventory.cs
@@ -1,5 +1,14 @@
+using System.Collections.Generic;
+
 namespace Meta.Inventory.FighterInventory {
-    public class FighterInventory {
+    public class FighterInventory : Inventory<OwnedCard> {
+        public override List<OwnedCard> inventory { get; set; }
         
+        
+        
+        
+        public override void CollectOperations(OwnedCard objInventoryItem) {
+            throw new System.NotImplementedException();
+        }
     }
 }
