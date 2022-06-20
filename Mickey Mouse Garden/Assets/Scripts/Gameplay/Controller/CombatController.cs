e437f68adf9af1609e43233b675d50d6428193b4
Jesper Danielsson <jesper.danielsson@student.forsbergsskola.se>
2022-06-20 11:56:40 +0200

    Basic command pattern for combat has been created

--- Mickey Mouse Garden/Assets/Scripts/Gameplay/Controller/CombatController.cs
+++ Mickey Mouse Garden/Assets/Scripts/Gameplay/Controller/CombatController.cs
@@ -1,11 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public class StrikeCommand : ICommand{
+   public void Execute(){
+      Debug.Log("hit");
+   }
+
+   public void Undo(){
+      throw new NotImplementedException();
+   }
+}
+
 public class CombatController : MonoBehaviour{
-   // private Fighter[] playerFighters;
-   // private Fighter[] enemyFighters;
-    
-    //TODO: use the command-pattern for this!
-    
+   private GameObject[] playerFighters;
+   private GameObject[] enemyFighters;
+
+   private Executor executor;
+
+   private void Awake(){
+      executor = FindObjectOfType<Executor>();
+   }
+
+   private void Update(){
+      if (Input.GetKeyDown(KeyCode.P)){
+         Debug.Log("Testing combat");
+         executor.Enqueue(new StrikeCommand());
+      }
+   }
 }
