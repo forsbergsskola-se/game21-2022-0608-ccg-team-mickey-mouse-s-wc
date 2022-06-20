7f752286f33f3ba1d43257256f232be1150e3b3c
Armin Afazeli <90157472+Armin-AF@users.noreply.github.com>
2022-06-20 16:06:32 +0200

    Update PurchasingSystem.cs

--- Mickey Mouse Garden/Assets/Scripts/Meta/ShoppingSystem/PurchasingSystem.cs
+++ Mickey Mouse Garden/Assets/Scripts/Meta/ShoppingSystem/PurchasingSystem.cs
@@ -1,16 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using Meta.Interfaces;
+using Meta.Inventory;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UDP;
 
-public class PurchasingSystem : MonoBehaviour, ICurrency{
-    public Inventory Inventory;
+public class PurchasingSystem<T> : MonoBehaviour, ICurrency where T : IInventoryItem{
+    public Inventory<T> Inventory;
+    private object shopItems;
     public string Name{ get; }
-    public int Amount{ get; }
+
+    public int Amount{ get; private set; }
+
     public string SpriteName{ get; }
     public Sprite Sprite{ get; }
     public void AddAmount(int value){
         throw new System.NotImplementedException();
+    }
+
+    public void BuyFromShop(Item item){
+        if (Amount - item.price < 0)
+        {
+            return;
+        }
+
+        Amount -= item.price;
+        RemoveItemFromList(shopItems, item);
+        //.Add(item);
+    }
+
+    private void RemoveItemFromList(object shopItems, Item item){
+        throw new System.NotImplementedException();
+    }
+}
+
+public class Item{
+    public int price;
+
+    public Item(int price){
+        this.price = price;
     }
 }
