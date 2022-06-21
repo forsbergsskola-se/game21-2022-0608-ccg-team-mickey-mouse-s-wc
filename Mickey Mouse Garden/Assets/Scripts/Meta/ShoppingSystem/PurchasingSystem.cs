0cf4ba9260506e050a3ccd17c60feee8f0015308
Armin Afazeli <90157472+Armin-AF@users.noreply.github.com>
2022-06-21 11:11:40 +0200

    UIShop Script + Error Fixed

--- Mickey Mouse Garden/Assets/Scripts/Meta/ShoppingSystem/PurchasingSystem.cs
+++ Mickey Mouse Garden/Assets/Scripts/Meta/ShoppingSystem/PurchasingSystem.cs
@@ -17,17 +17,7 @@ public class PurchasingSystem<T> : MonoBehaviour where T : IInventoryItem{
     public void AddAmount(int value){
         throw new System.NotImplementedException();
     }
-
-    public void BuyFromShop(Item item){
-        if (Amount - item.price < 0)
-        {
-            return;
-        }
-
-        Amount -= item.price;
-        RemoveItemFromList(shopItems, item);
-        //.Add(item);
-    }
+    
 
     private void RemoveItemFromList(object shopItems, Item item){
         throw new System.NotImplementedException();
---
+++ Mickey Mouse Garden/Assets/Scripts/Meta/ShoppingSystem/UIShop.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIShop : MonoBehaviour{
+    private Transform container;
+    private Transform shopItemTemplate;
+
+    private void Awake(){
+        container = transform.Find("container");
+        shopItemTemplate = container.Find("shopItemTemplate");
+        shopItemTemplate.gameObject.SetActive(false);
+    }
+}
