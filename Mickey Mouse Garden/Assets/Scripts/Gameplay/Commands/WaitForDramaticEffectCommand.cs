0345c61f86b735fb2a3b5ccd4035fefc79741aec
Jesper Danielsson <jesper.danielsson@student.forsbergsskola.se>
2022-06-22 11:04:30 +0200

    safe push before testing async command patten

--- Mickey Mouse Garden/Assets/Scripts/Gameplay/Commands/WaitForDramaticEffectCommand.cs
+++ Mickey Mouse Garden/Assets/Scripts/Gameplay/Commands/WaitForDramaticEffectCommand.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Threading.Tasks;
+using System.Timers;
 using UnityEngine;
 
 public class WaitForDramaticEffectCommand : ICommand{
     private int duration;
+    Timer timer = new();
+
 
     public WaitForDramaticEffectCommand(int duration){
         this.duration = duration;
@@ -11,18 +14,30 @@ public class WaitForDramaticEffectCommand : ICommand{
     
     //TODO: WIP, currently waiting in the task not for the task, command pattern needs to be async for that.
 
-    public void Execute(){
-        Wait();
+    public async void Execute(){
+        await Wait();
     }
 
-    private void Wait(){
-    var t = Task.Run(async delegate{
-        await Task.Delay(duration * 1000);
-        Task.Yield();
-    });
+    private async Task Wait(){
+        timer.Elapsed += CountDown;
+        timer.Interval = 1000;
+        timer.Start();
     }
-    
-    
+
+    private void CountDown(object sender, EventArgs e){
+        if (duration == 0)
+        {
+            timer.Stop();
+
+        }
+        else if(duration > 0)
+        {
+            duration--;
+            Debug.Log(duration);
+        }
+    }
+
+
     public void Undo(){
         throw new NotImplementedException();
     }
