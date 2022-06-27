db1259b5c50b23d485dafe6320b6966746b9a3af
Jasmine Öhlin <jasmine.ohlin@student.forsbergsskola.se>
2022-06-27 13:07:01 +0200

    Grow slot timer updated to use datetime

--- Mickey Mouse Garden/Assets/Scripts/Meta/Inventory/SeedInventory/GrowSlot.cs
+++ Mickey Mouse Garden/Assets/Scripts/Meta/Inventory/SeedInventory/GrowSlot.cs
@@ -15,35 +15,41 @@ namespace Meta.Inventory {
         [SerializeField] private TextMeshProUGUI growthTimerText;
         [SerializeField] private Sprite seedSprite;
 
+        private DateTime plantTime;
+        private DateTime HarvestTime => plantTime.Add(new TimeSpan(0, 0, Mathf.RoundToInt(growTime)));
+
+        private float TimeUntilHarvest {
+            get {
+                TimeSpan timeSpanRemaining = HarvestTime.Subtract(DateTime.Now);
+                float secondsRemaining = (float)timeSpanRemaining.TotalSeconds;
+                return Mathf.Max(secondsRemaining, 0);
+            }
+        }
+        
         private string rarityText;
-        private float timeUntilToHarvest;
 
         //TODO: Make slider not clickable
         private void Awake() {
+            plantTime = DateTime.Now;
             rarityText = rarityType.ToString();
             timeSlider.maxValue = growTime;
             timeSlider.value = growTime;
-            timeUntilToHarvest = growTime;
         }
 
         private void Update() {
             if (readyToHarvest) return;
             
-            if (timeUntilToHarvest > 0) {
-                timeUntilToHarvest -= Time.deltaTime;
-            } else {
+            if (DateTime.Now > HarvestTime) {
                 readyToHarvest = true;
-                timeUntilToHarvest = 0;
-                timeSlider.value = timeUntilToHarvest;
                 SendReadyToHarvestMessage();
             }
-
-            UpdateTimerText(timeUntilToHarvest);
-            timeSlider.value = timeUntilToHarvest;
+            
+            UpdateTimerText(TimeUntilHarvest);
+            timeSlider.value = TimeUntilHarvest;
         }
 
         private void UpdateTimerText(float timeLeft) {
-            growthTimerText.text = $"{rarityText} - {timeLeft.ToString()}";
+            growthTimerText.text = $"{rarityText} - {timeLeft.ToString("0")}"; //TODO: Talk w designers for the format
         }
 
         private void SendReadyToHarvestMessage() {
