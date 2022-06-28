548f843d5d734e6b5ad254e80dce950f0e9a2330
Jesper Danielsson <jesper.danielsson@student.forsbergsskola.se>
2022-06-28 11:07:16 +0200

    Commented the combattester so its sligthly easier to use and understand

--- Mickey Mouse Garden/Assets/Scripts/Gameplay/TESTS/CombatTester.cs
+++ Mickey Mouse Garden/Assets/Scripts/Gameplay/TESTS/CombatTester.cs
@@ -7,12 +7,15 @@ public class CombatTester : MonoBehaviour{
     private int id;
 
     private SelectedFighterTeamMessage MakeAFighterTeam(bool playerteam){
+        //Creating the message and the stack.
         var team = new SelectedFighterTeamMessage();
         var fighterStack = new Stack<FighterInfo>();
        
+        //pushing all the singularfighters into the stack
         for (int i = 0; i < 3; i++){
             fighterStack.Push(createFighterInfo());
         }
+        //set values for the team and the bool if it's the players team or the opponents.
         team.FighterTeam = fighterStack;
         team.PlayerTeam = playerteam;
         return team;
@@ -20,17 +23,22 @@ public class CombatTester : MonoBehaviour{
 
     private void Update(){
         if (Input.GetKeyDown(KeyCode.F)){
+            //when button F is pressed the playerteam is selected and then the message is sent over to actual combat.
             var playerTeam = MakeAFighterTeam(true);
             Broker.InvokeSubscribers(typeof(SelectedFighterTeamMessage), playerTeam);
         }
         if (Input.GetKeyDown(KeyCode.G)){
+            //when button G is pressed the enemy team is selected and then the message is sent over to actual combat.
             var enemyTeam = MakeAFighterTeam(false);
             Broker.InvokeSubscribers(typeof(SelectedFighterTeamMessage), enemyTeam);
         }
     }
 
     private FighterInfo createFighterInfo(){
+        //since fighterInfo is monobehaviour you have to create it inside the message when testing.
         var fighterMessage = new FighterMessage{
+            
+            //This is where the actual fighter values are assigned, this will be taken from the inventory when selected.
             fighterInfo = new FighterInfo{ // this is for testing, dont change even though its recommended.
                 ID = id++,
                 MaxHealth = 10,
