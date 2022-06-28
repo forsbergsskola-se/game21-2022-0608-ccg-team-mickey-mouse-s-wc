77827e76c46580e87789012d71ea8db0dde2708b
Jasmine Öhlin <jasmine.ohlin@student.forsbergsskola.se>
2022-06-28 11:39:10 +0200

    Constructor added to new seed

--- Mickey Mouse Garden/Assets/Scripts/Meta/Inventory/NewSeedInventory/NewSeed.cs
+++ Mickey Mouse Garden/Assets/Scripts/Meta/Inventory/NewSeedInventory/NewSeed.cs
@@ -6,6 +6,14 @@ namespace Meta.Inventory.NewSeedInventory {
     [System.Serializable]
     public class NewSeed : MonoBehaviour, IInventoryItem, ISaveData {
         public Rarity rarity;
+        public Rarity Rarity { get; set; }
+        public DateTime HarvestTime { get; set; }
+        public bool ReadyToHarvest { get; set; } //TODO: Check if timer handles setting bool correctly or if seed needs to set
+        public StringGUID ID { get; }
+
+        public NewSeed() {
+            ID = new StringGUID().NewGuid();
+        }
 
         private void Awake() {
             Rarity = rarity;
@@ -19,11 +27,6 @@ namespace Meta.Inventory.NewSeedInventory {
             Destroy(gameObject); //<--- Not needed
         }
         
-        public Rarity Rarity { get; set; }
-        public DateTime HarvestTime { get; set; }
-        public bool ReadyToHarvest { get; set; }
-        public StringGUID ID { get; }
-
         public void TryLoadData() {
             throw new System.NotImplementedException();
         }
