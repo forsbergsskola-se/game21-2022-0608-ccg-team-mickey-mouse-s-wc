3d3bbac4c107ce7d80bee3649b8cef221cdd958b
Oliver Källerfors <idetorka@hotmail.com>
2022-06-29 15:14:17 +0200

    Fixed Compile Issue
    Co-Authored-By: Armin Afazeli <90157472+Armin-AF@users.noreply.github.com>

--- Mickey Mouse Garden/Assets/Scripts/Meta/Inventory/Song/SongInventory.cs
+++ Mickey Mouse Garden/Assets/Scripts/Meta/Inventory/Song/SongInventory.cs
@@ -1,11 +1,13 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Meta.Inventory;
 using UnityEngine;
-
+[Serializable]
 public class SongInventory : Inventory<Song>
 {
-    public override List<Song> Items{ get; set; }
+    public override InventoryList<Song> InventoryList{ get; set; }
+
     public override void CollectOperations(Song addedItem){
         throw new System.NotImplementedException();
     }
