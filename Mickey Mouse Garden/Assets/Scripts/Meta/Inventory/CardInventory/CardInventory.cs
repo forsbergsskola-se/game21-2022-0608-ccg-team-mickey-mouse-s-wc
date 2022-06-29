177549ae58632c81cf765e4bab2cc7b14c58083b
Jasmine Öhlin <jasmine.ohlin@student.forsbergsskola.se>
2022-06-29 16:40:44 +0200

    Update CardInventory.cs

--- Mickey Mouse Garden/Assets/Scripts/Meta/Inventory/CardInventory/CardInventory.cs
+++ Mickey Mouse Garden/Assets/Scripts/Meta/Inventory/CardInventory/CardInventory.cs
@@ -9,7 +9,7 @@ namespace Meta.Inventory.FighterInventory {
     /// </summary>
     [System.Serializable]
     public class CardInventory : Inventory<Card> {
-        public override InventoryList<Card> InventoryList { get; set; }
+        public override InventoryList<Card> InventoryList { get; set; } = new InventoryList<Card>();
         public static CardInventory Instance { get; private set; }
 
         private void Awake() {
