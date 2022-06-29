7824591d5c5bb71ec966902bf3badcdf24fbbf19
Jesper Danielsson <jesper.danielsson@student.forsbergsskola.se>
2022-06-29 14:18:14 +0200

    Serialized the level for clarity
    Co-Authored-By: Jasmine Öhlin <90619375+JasmineOhlin@users.noreply.github.com>

--- Mickey Mouse Garden/Assets/Scripts/Meta/Combat/CombatSelection/Level.cs
+++ Mickey Mouse Garden/Assets/Scripts/Meta/Combat/CombatSelection/Level.cs
@@ -2,8 +2,8 @@ using Meta.Cards;
 using UnityEngine;
 
 public class Level : MonoBehaviour {
-    public string levelName;
-    public CardConfig[] enemyTeamMembers;
+    [SerializeField] private string levelName;
+    [SerializeField] private CardConfig[] enemyTeamMembers; //TODO: force amount of elements to be 3, pesky designers.
     
     public void ConfirmTeam(){
         LevelMessage msg = new LevelMessage{Team = enemyTeamMembers};
