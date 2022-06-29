a9169dcfdb163c2fb8b2a0fb44aacc6531655bca
Jasmine Öhlin <jasmine.ohlin@student.forsbergsskola.se>
2022-06-29 14:38:41 +0200

    Serialization and json added to InventoryList

--- Mickey Mouse Garden/Assets/Scripts/Meta/Inventory/InventoryList.cs
+++ Mickey Mouse Garden/Assets/Scripts/Meta/Inventory/InventoryList.cs
@@ -1,8 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Meta.Interfaces;
+using Newtonsoft.Json;
 
 namespace Meta.Inventory {
+    [System.Serializable]
+    [JsonObject]
     public abstract class InventoryList<T> : ISaveData, IEnumerable where T : IInventoryItem {
         public List<T> Items { get; set; }
         public StringGUID ID { get; }
