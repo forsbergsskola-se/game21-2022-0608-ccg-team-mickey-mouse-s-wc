d27bcb18a1359e02d0959d057554dee38278ad76
Benjamin Ting <tingzhb@gmail.com>
2022-06-30 14:23:39 +0200

    Add sound to Main Scene

---
+++ Mickey Mouse Garden/Assets/Scripts/Gameplay/NavigationRoom/MainSceneMessageListener.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class MainSceneMessageListener : MonoBehaviour{
+    private MainSceneSoundManager mainSceneSoundManager;
+    private void Awake(){
+        mainSceneSoundManager = GetComponent<MainSceneSoundManager>();
+        Broker.Subscribe<UIChangedMessage>(OnUIChangedMessageReceived);
+        mainSceneSoundManager.PlayMusic();
+    }
+    private void OnUIChangedMessageReceived(UIChangedMessage obj){
+        mainSceneSoundManager.MainClick();
+        switch (obj.ObjectTag){
+            // pShop
+            case "PShop":
+                mainSceneSoundManager.ModulateMusic(4);
+                break;
+            // shop
+            case "Shop":
+                mainSceneSoundManager.ModulateMusic(3);
+                break;
+            // shed
+            case "Shed":
+                mainSceneSoundManager.ModulateMusic(2);
+                break;
+            // greenhouse
+            
+            case "Garden":
+                mainSceneSoundManager.ModulateMusic(1);
+                break;
+
+            // arena
+            case "Arena":
+                mainSceneSoundManager.StopMusic();
+                mainSceneSoundManager.PlayPreCombatMusic();
+                break;
+
+            // arena2
+            case "Arena2":
+                mainSceneSoundManager.StopMusic();
+                // mainSceneSoundManager.PlayPreCombatMusic();
+                break;
+        }
+    }
+    
+}
---
+++ Mickey Mouse Garden/Assets/Sound/Scripts/MainSceneSoundManager.cs
@@ -0,0 +1,58 @@
+using FMOD.Studio;
+using UnityEngine;
+
+public class MainSceneSoundManager : MonoBehaviour
+{
+  [Header("Music and Ambience")] 
+  [SerializeField] private FMODUnity.EventReference ambientMusic, preCombatMusic;
+
+  private EventInstance ambientMusicInstance, preCombatMusicInstance;
+  
+  
+  public void PlayMusic(){
+    ambientMusicInstance = FMODUnity.RuntimeManager.CreateInstance(ambientMusic);
+    ambientMusicInstance.start();
+  }
+  public void PlayPreCombatMusic(){
+    preCombatMusicInstance = FMODUnity.RuntimeManager.CreateInstance(preCombatMusic);
+    preCombatMusicInstance.start();
+  }
+  public void plantSeeds() 
+  { 
+    FMODUnity.RuntimeManager.PlayOneShot("event:/Meta/PlantSeeds");
+  }
+
+  public void MainClick() {
+    FMODUnity.RuntimeManager.PlayOneShot("event:/UI/MainClick");
+  }
+  
+  public void purchase()
+  { 
+    FMODUnity.RuntimeManager.PlayOneShot("event:/UI/Market/Purchase");
+  }
+  
+  public void Swoosh() { 
+    FMODUnity.RuntimeManager.PlayOneShot("event:/UI/Swosh");
+  }
+
+  public void sell()
+  {
+    FMODUnity.RuntimeManager.PlayOneShot("event:/UI/Market/Sell");
+  }
+
+  public void harvest()
+  {
+    FMODUnity.RuntimeManager.PlayOneShot("event:/Meta/Market");
+  }
+
+
+  public void StopMusic(){
+    ambientMusicInstance.stop(STOP_MODE.ALLOWFADEOUT);
+  }
+  public void StopPreCombatMusic(){
+    preCombatMusicInstance.stop(STOP_MODE.ALLOWFADEOUT);
+  }
+  public void ModulateMusic(int sceneNum){
+    FMODUnity.RuntimeManager.StudioSystem.setParameterByName("Delta", sceneNum);
+  }
+}
