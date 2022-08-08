d6e219e4e2d02fee880fa0ebb41354baf8462d70
Benjamin Ting <tingzhb@gmail.com>
2022-08-08 11:21:49 +0200

    Move Reward formula.

--- Mickey Mouse Garden/Assets/Scripts/Meta/Combat/CombatSelection/Level.cs
+++ Mickey Mouse Garden/Assets/Scripts/Meta/Combat/CombatSelection/Level.cs
@@ -1,12 +1,25 @@
+using System;
 using Meta.Cards;
 using UnityEngine;
 
 public class Level : MonoBehaviour {
-    [SerializeField] private string levelName;
+    [SerializeField] private float levelNumber, levelMultiplier = 100;
     [SerializeField] private CardConfig[] enemyTeamMembers; //TODO: force amount of elements to be 3, pesky designers.
-    
+    private PlayerLevel playerLevel;
+    private int reward;
     public void ConfirmTeam(){
-        LevelMessage msg = new LevelMessage{Team = enemyTeamMembers};
+        LevelMessage msg = new(){
+            Team = enemyTeamMembers,
+            Reward = reward,
+            Level = (int)levelNumber,
+            
+        };
         Broker.InvokeSubscribers(typeof(LevelMessage), msg);
+    }
+
+    private void Start(){
+        playerLevel = FindObjectOfType<Player>().GetComponent<PlayerLevel>();
+        reward = (int)(levelNumber / playerLevel.Level * levelMultiplier * levelNumber);
+        Debug.Log(reward);
     }
 }
