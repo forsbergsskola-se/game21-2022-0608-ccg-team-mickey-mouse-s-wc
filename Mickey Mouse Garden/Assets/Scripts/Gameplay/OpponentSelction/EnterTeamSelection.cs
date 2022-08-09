44f732ec6b51e0207f699e1306b070ee503e5a95
Benjamin Ting <tingzhb@gmail.com>
2022-08-09 12:50:25 +0200

    Lock Opponent Selection UI

--- Mickey Mouse Garden/Assets/Scripts/Gameplay/OpponentSelction/EnterTeamSelection.cs
+++ Mickey Mouse Garden/Assets/Scripts/Gameplay/OpponentSelction/EnterTeamSelection.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class EnterTeamSelection : MonoBehaviour{
@@ -6,18 +7,28 @@ public class EnterTeamSelection : MonoBehaviour{
 	
 	private bool shown;
 
+
+	private void Awake(){
+		Broker.Subscribe<UILockMessage>(OnUILockedMessageReceived);
+	}
+	private void OnUILockedMessageReceived(UILockMessage obj){
+		shown = obj.Locked;
+	}
+
 	private void OnMouseDown() {
-		if (GetComponent<MatchInformation>().isUnlocked) {
+		if (GetComponent<MatchInformation>().isUnlocked && !shown) {
 			confirmationBox.SetActive(true);
-			shown = true;
+			LockUI(); 
 			GetComponent<MatchInformation>().ConfirmTeam();
-		} else {
+		} else if (!shown) {
 			levelNotUnlockedBox.SetActive(true);
+			LockUI();
 		}
 	}
 
 	public void AcceptNotUnlocked() {
 		levelNotUnlockedBox.SetActive(false);
+		UnLockUI();
 	}
 	
 	public void AnswerYes(){
@@ -27,9 +38,24 @@ public class EnterTeamSelection : MonoBehaviour{
 
 	public void AnswerNo(){
 		confirmationBox.SetActive(false);
+		UnLockUI();
 	}
 	
 	private void GoToTeamSelection(){
 		teamSelectCanvas.GetComponent<Canvas>().enabled = true;
+	}
+
+	private void LockUI(){
+		UILockMessage uiLockMessage = new(){
+			Locked = true
+		};
+		Broker.InvokeSubscribers(typeof(UILockMessage), uiLockMessage);
+	}
+
+	private void UnLockUI(){
+		UILockMessage uiLockMessage = new(){
+			Locked = false
+		};
+		Broker.InvokeSubscribers(typeof(UILockMessage), uiLockMessage);
 	}
 }
