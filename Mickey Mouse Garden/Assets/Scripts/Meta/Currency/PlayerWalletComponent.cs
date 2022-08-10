f0ab414b7ba8930fb3a3954d83091b6175f9f262
Jasmine Öhlin <jasmine.ohlin@student.forsbergsskola.se>
2022-08-10 14:04:55 +0200

    Added method to test bigger amount of currency

--- Mickey Mouse Garden/Assets/Scripts/Meta/Currency/PlayerWalletComponent.cs
+++ Mickey Mouse Garden/Assets/Scripts/Meta/Currency/PlayerWalletComponent.cs
@@ -93,5 +93,17 @@ namespace Experiment{
             message.fertilizer = fertilizer;
             Broker.InvokeSubscribers(typeof(AddPlayerCurrencyMessage), message);
         }
+        
+        [ContextMenu("TestAddALotOfCurrency")]
+        public void TestAddMegaCurrency(){
+            var money = new Money();
+            money.Amount = 100;
+            var fertilizer = new Fertilizer();
+            fertilizer.Amount = 200;
+            var message = new AddPlayerCurrencyMessage();
+            message.money = money;
+            message.fertilizer = fertilizer;
+            Broker.InvokeSubscribers(typeof(AddPlayerCurrencyMessage), message);
+        }
     }
 }
