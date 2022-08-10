9574b5f81bc4ea3169f697f45abc3a16d3148035
Jesper Danielsson <jesper.danielsson@student.forsbergsskola.se>
2022-08-10 11:11:22 +0200

    Fixed error of multiple rounds of combat not possible

--- Mickey Mouse Garden/Assets/Scripts/Gameplay/View/CardCreator.cs
+++ Mickey Mouse Garden/Assets/Scripts/Gameplay/View/CardCreator.cs
@@ -17,7 +17,6 @@ public class CardCreator : MonoBehaviour{
         FighterInfo[] cards = new FighterInfo[3];
         obj.FighterTeam.CopyTo(cards,0);
         for (int i = cards.Length - 1; i >= 0; i--){
-            Debug.Log(i);
             fighter = cards[i];
             // Spawns card at incremental card slots starting from 1 (not 0).
             InstantiateFighter(fighter, cardSlots[cardCount]);
