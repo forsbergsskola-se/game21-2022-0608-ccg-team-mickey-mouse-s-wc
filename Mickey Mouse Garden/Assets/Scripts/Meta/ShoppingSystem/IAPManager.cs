94dcfa391e71423cbeabc750a781f2c26073a935
Oliver Källerfors <idetorka@hotmail.com>
2022-08-16 13:51:32 +0200

    renamed Id's in IAPManager

--- Mickey Mouse Garden/Assets/Scripts/Meta/ShoppingSystem/IAPManager.cs
+++ Mickey Mouse Garden/Assets/Scripts/Meta/ShoppingSystem/IAPManager.cs
@@ -7,24 +7,24 @@ using UnityEngine.Purchasing;
 public class IAPManager : MonoBehaviour
 {
     
-    private string Fertilizerx10 = "10xFertilizer";
-    private string Fertilizerx60 = "60xFertilizer";
-    private string Fertilizerx120 = "120xFertilizer";
-    private string Fertilizerx650 = "650xFertilizer";
+    private string fertilizer_10 = "fertilizer_10";
+    private string fertilizer_60 = "fertilizer_60";
+    private string fertilizer_120 = "fertilizer_120";
+    private string fertilizer_650 = "fertilizer_650";
     
 
     public void OnPurchaseComplete(Product product){
         var reward = new Fertilizer();
-        if (product.definition.id == Fertilizerx10){
+        if (product.definition.id == fertilizer_10){
             reward.Amount = 10;
         }
-        if (product.definition.id == Fertilizerx60){
+        if (product.definition.id == fertilizer_60){
             reward.Amount = 60;
         }
-        if (product.definition.id == Fertilizerx120){
+        if (product.definition.id == fertilizer_120){
             reward.Amount = 120;
         }
-        if (product.definition.id == Fertilizerx650){
+        if (product.definition.id == fertilizer_650){
             reward.Amount = 650;
         }
         Debug.Log(reward.Amount);
