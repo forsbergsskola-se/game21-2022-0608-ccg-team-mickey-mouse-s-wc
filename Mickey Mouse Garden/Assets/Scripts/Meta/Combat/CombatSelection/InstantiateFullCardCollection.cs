3ce58d59e372c120b0176fbef4dacf6b5527172a
Benjamin Ting <tingzhb@gmail.com>
2022-08-16 14:06:46 +0200

    Prevent card in card slot from being chosen

--- Mickey Mouse Garden/Assets/Scripts/Meta/Combat/CombatSelection/InstantiateFullCardCollection.cs
+++ Mickey Mouse Garden/Assets/Scripts/Meta/Combat/CombatSelection/InstantiateFullCardCollection.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using Meta.Cards;
 using Meta.Inventory.FighterInventory;
@@ -6,12 +7,23 @@ using UnityEngine;
 public class InstantiateFullCardCollection : MonoBehaviour{
     [SerializeField] private GameObject cardButtonPrefab;
     private List<Card> playerCardTeam;
+    private Card originCard;
     private void OnEnable(){
+        Broker.Subscribe<CardSelectionMessage>(OnCardSelectionMessageReceived);
         playerCardTeam = FindObjectOfType<CardInventory>().InventoryList.Items;
-        for (int i = 0; i < playerCardTeam.Count; i++){
-            var instance = Instantiate(cardButtonPrefab, gameObject.transform);
-            instance.GetComponentInChildren<CardView>().Configure(playerCardTeam[i]);
+        StartCoroutine(DisplayValidCards());
+    }
+    private IEnumerator DisplayValidCards(){
+        yield return new WaitForSeconds(0.1f);
+        foreach (var card in playerCardTeam){
+            if (card.ID != originCard.ID){
+                var instance = Instantiate(cardButtonPrefab, gameObject.transform);
+                instance.GetComponentInChildren<CardView>().Configure(card);
+            }
         }
+    }
+    private void OnCardSelectionMessageReceived(CardSelectionMessage obj){
+        originCard = obj.Card;
     }
     private void OnDisable(){
         var cards = GetComponentsInChildren<CardView>();
