24ed093b86d90c5db26d961c2b2efe1a0797ed33
Oliver Källerfors <idetorka@hotmail.com>
2022-08-17 11:06:29 +0200

    Created Sort Methods by Rarity And Alignment

--- Mickey Mouse Garden/Assets/Scripts/Meta/Combat/CombatSelection/InstantiateFullCardCollection.cs
+++ Mickey Mouse Garden/Assets/Scripts/Meta/Combat/CombatSelection/InstantiateFullCardCollection.cs
@@ -8,14 +8,28 @@ public class InstantiateFullCardCollection : MonoBehaviour{
     [SerializeField] private GameObject cardButtonPrefab;
     private List<Card> playerCardTeam;
     private void OnEnable(){
+        Broker.Subscribe<SortCardInventoryByRarityMessage>(OnSortCardInventoryByRarityMessageReceived);
+        Broker.Subscribe<SortCardInventoryByAlignmentMessage>(OnSortCardInventoryByAlignmentMessageReceived);
         playerCardTeam = FindObjectOfType<CardInventory>().InventoryList.Items;
         for (int i = 0; i < playerCardTeam.Count; i++){
             var instance = Instantiate(cardButtonPrefab, gameObject.transform);
             instance.GetComponentInChildren<CardView>().Configure(playerCardTeam[i]);
         }
     }
-    
+
+    void OnSortCardInventoryByAlignmentMessageReceived(SortCardInventoryByAlignmentMessage obj){
+        OnDisable();
+        OnEnable();
+    }
+
+    void OnSortCardInventoryByRarityMessageReceived(SortCardInventoryByRarityMessage obj){
+        OnDisable();
+        OnEnable();
+    }
+
     private void OnDisable(){
+        Broker.Unsubscribe<SortCardInventoryByRarityMessage>(OnSortCardInventoryByRarityMessageReceived);
+        Broker.Unsubscribe<SortCardInventoryByAlignmentMessage>(OnSortCardInventoryByAlignmentMessageReceived);
         var cards = GetComponentsInChildren<CardView>();
         foreach (var card in cards){
             Destroy(card.gameObject);
--- Mickey Mouse Garden/Assets/Scripts/Meta/Inventory/CardInventory/CardInventory.cs
+++ Mickey Mouse Garden/Assets/Scripts/Meta/Inventory/CardInventory/CardInventory.cs
@@ -13,11 +13,35 @@ namespace Meta.Inventory.FighterInventory {
     public class CardInventory : Inventory<Card> {
         public override InventoryList<Card> InventoryList { get; set; } = new InventoryList<Card>(new StringGUID().CreateStringGuid(10101));
         public static CardInventory Instance { get; private set; }
+
+        public override void Awake(){
+            base.Awake();
+            Broker.Subscribe<SortCardInventoryByRarityMessage>(SortByRarity);
+            Broker.Subscribe<SortCardInventoryByAlignmentMessage>(SortByAlignment);
+        }
+
+        public override void OnDisable(){
+            base.OnDisable();
+            Broker.Unsubscribe<SortCardInventoryByRarityMessage>(SortByRarity);
+            Broker.Subscribe<SortCardInventoryByAlignmentMessage>(SortByAlignment);
+        }
+
         public override void CollectOperations(Card addedItem) {
             base.CollectOperations(addedItem);
             
             var cardAddedMessage = new CardAddedToInventoryMessage(addedItem);
             Broker.InvokeSubscribers(cardAddedMessage.GetType(), cardAddedMessage);
+        }
+
+
+        public void SortByRarity(SortCardInventoryByRarityMessage sortCardInventoryByRarityMessage){
+            InventoryList.SortByRarity(0, InventoryList.Items.Count - 1);
+            InventoryList.Save();
+        }
+
+        public void SortByAlignment(SortCardInventoryByAlignmentMessage sortCardInventoryByAlignmentMessage){
+            InventoryList.SortByAlignment(0, InventoryList.Items.Count - 1);
+            InventoryList.Save();
         }
     }
 }
@@ -31,7 +55,7 @@ public static class CardInventoryExtensions {
     /// <param name="leftIndex"></param>
     /// <param name="rightIndex"></param>
     /// <exception cref="Exception"></exception>
-    public static void SortByRarityASC(this InventoryList<Card> inventory, int leftIndex, int rightIndex) {
+    public static void SortByRarity(this InventoryList<Card> inventory, int leftIndex, int rightIndex) {
         var i = leftIndex; 
         var j = rightIndex;
         var pivot = inventory.Items[leftIndex].Rarity;
@@ -46,18 +70,55 @@ public static class CardInventoryExtensions {
 
             if (i <= j){
                 //Swaps the cards
-                (inventory.Items[i].Rarity, inventory.Items[j].Rarity) = (inventory.Items[j].Rarity, inventory.Items[i].Rarity);
+                (inventory.Items[i], inventory.Items[j]) = (inventory.Items[j], inventory.Items[i]);
                 i++;
                 j--;
             }
         }
         
         if(leftIndex < j) {
-            SortByRarityASC(inventory, leftIndex, j);
+            SortByRarity(inventory, leftIndex, j);
         }
         
         if(i < rightIndex) {
-            SortByRarityASC(inventory, i, rightIndex);
+            SortByRarity(inventory, i, rightIndex);
+        }
+    }
+    
+    /// <summary>
+    /// Quicksort. Left index should be 0, right index should be lenght of list - 1.
+    /// </summary>
+    /// <param name="inventory"></param>
+    /// <param name="leftIndex"></param>
+    /// <param name="rightIndex"></param>
+    /// <exception cref="Exception"></exception>
+    public static void SortByAlignment(this InventoryList<Card> inventory, int leftIndex, int rightIndex) {
+        var i = leftIndex; 
+        var j = rightIndex;
+        var pivot = inventory.Items[leftIndex].Alignment;
+
+        while (i <= j){
+            while(inventory.Items[i].Alignment < pivot) {
+                i++;
+            }
+            while(inventory.Items[j].Alignment > pivot) {
+                j--;
+            }
+
+            if (i <= j){
+                //Swaps the cards
+                (inventory.Items[i], inventory.Items[j]) = (inventory.Items[j], inventory.Items[i]);
+                i++;
+                j--;
+            }
+        }
+        
+        if(leftIndex < j) {
+            SortByAlignment(inventory, leftIndex, j);
+        }
+        
+        if(i < rightIndex) {
+            SortByAlignment(inventory, i, rightIndex);
         }
     }
 }
