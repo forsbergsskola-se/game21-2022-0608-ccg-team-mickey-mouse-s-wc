ef0ad76bd68e4a373907af624037f8aa9ac9329c
Jasmine Öhlin <jasmine.ohlin@student.forsbergsskola.se>
2022-08-18 10:12:24 +0200

    Removed IController & IntSO (unused scripts)

--- Mickey Mouse Garden/Assets/Scripts/Meta/IntSO.cs
+++
@@ -1,7 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-[CreateAssetMenu(fileName = "New Int So", menuName = "Base Types/Int So")]
-public class IntSO : ScriptableObject{
-    public int intSo;
-}
